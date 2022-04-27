ee1b7e104df7a2ecc58c6156d7c5e263fdbf65c8
mrbrightsiide <devkimsia@gmail.com>
2022-04-28 01:12:06 +0900

    Add diary detail page

---
+++ src/util/emotion.js
@@ -0,0 +1,29 @@
+const env = process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || "";
+export const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_caption: "완전 좋아요",
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_caption: "좋아요",
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_caption: "그럭저럭",
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_caption: "별로에요",
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_caption: "끔찍해요",
+  },
+];
